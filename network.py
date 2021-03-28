56e51a3fa2f61a9a2ac0a679a98ed553062a7354
kwyosu27@gmail.com <kwyosu27@gmail.com>
2021-03-28 17:55:55 +0900

    edit wrong code

--- network.py
+++ network.py
@@ -3,7 +3,7 @@ import pandas as pd
 import networkx as nx
 
 class traffic_Network:
-    def __ini__(self, G):
+    def __init__(self, G):
         self.network = G
         
     def neighbor_link_weight(self, edge, edge_weight='weight'):
@@ -24,13 +24,26 @@ class traffic_Network:
                     +list(self.network.out_edges(edge[1], data=edge_weight))
         # 자기 자신이 두번 불러와지므로, 2번 제거
         link = (edge[0], edge[1], self.network.get_edge_data(edge[0], edge[1],)[0][edge_weight])
-        for i in range(2):neighbor_link.remove(input_link)
+        for i in range(2):neighbor_link.remove(link)
         return neighbor_link
     
-    def mean_wegiht_neighbor(self, edge, edge_weight='weight'):
+    def one_direction_neighbor_link_weight(self, edge, edge_weight='weight'):
+        """
+        neighbor_link_weight 함수에서 같은 방향으로 이어진 이웃들의 특성 가져오기
+        -> -> ->
+        """
+        neighbor_link = list(self.network.in_edges(edge[0], data=edge_weight))\
+                    +list(self.network.out_edges(edge[1], data=edge_weight))
+        return neighbor_link
+    
+    def mean_wegiht_neighbor(self, edge, edge_weight='weight', direction = 'bi'):
         """
         입력된 edge에 연결된 링크들의 weight의 평균
         """
-        return np.mean(np.array(neighbor_link_weight(edge, edge_weight)[:,2]))
+        if direction == 'bi':
+            neighbor_link_list = np.array(self.neighbor_link_weight(edge, edge_weight))
+        elif direction == 'one':
+            neighbor_link_list = np.array(self.one_direction_neighbor_link_weight(edge, edge_weight))
+        return np.mean(np.array(neighbor_link_list)[:,2])
     
     
